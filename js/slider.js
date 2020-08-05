617a2acd75ff494b4c9a9275dfe7f80506e95835
ZeroVlad <57874653+ZeroVlad@users.noreply.github.com>
2020-08-05 17:03:16 +0300

    new

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.slider').slick({
+    speed: 1000,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: false,
+    pauseOnDotsHover: false
+  });
+});
