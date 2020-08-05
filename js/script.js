183119f6c718e7f25eb72bbcec346fd386bf3191
ZeroVlad <57874653+ZeroVlad@users.noreply.github.com>
2020-08-05 20:52:53 +0300

    add adaptive

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuIcon = document.querySelector('.menu-icon');
+const menu = document.querySelector('.drop-menu');
+const menuLinks = document.querySelectorAll('.drop-menu a');
+
+menuBtn.onclick = () => {
+	menuIcon.classList.toggle('active');
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+	if (menu.classList.contains('active')) {
+		document.body.style.overflow='hidden';
+	}
+	else{
+		document.body.style.overflow='visible';
+	}
+}
+
+for (let index = 0; index < menuLinks.length; index++) {
+	let menuLink = menuLinks[index];
+	menuLink.addEventListener('click', () => {
+		menuIcon.classList.toggle('active');
+		menuBtn.classList.toggle('active');
+		menu.classList.toggle('active');
+		if (menu.classList.contains('active')) {
+			document.body.style.overflow='hidden';
+		}
+		else{
+			document.body.style.overflow='visible';
+		}
+	});
+}
